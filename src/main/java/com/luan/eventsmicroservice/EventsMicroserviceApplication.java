91cead854cc109d467af4f5e7bac321949b40043
luan <luanbianchine1@gmail.com>
2023-12-21 15:25:53 -0300

    feat: enable FeignClient in app

--- src/main/java/com/luan/eventsmicroservice/EventsMicroserviceApplication.java
+++ src/main/java/com/luan/eventsmicroservice/EventsMicroserviceApplication.java
@@ -2,8 +2,10 @@ package com.luan.eventsmicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class EventsMicroserviceApplication {
 
 	public static void main(String[] args) {
